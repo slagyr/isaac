1a48b54c50fbad59246e05f854f3c5694e247a7a
Micah <micahmartin@gmail.com>
2026-01-28 15:20:13 -0700

    Replace hardcoded pg-root with config-based database connection
    - pg-root now derives from (:db config/active) with dbname='postgres'
    - save :postgres method now uses (:db config/active) directly
    - Tests use with-config to set up test database config
    - Removed hardcoded pg-root and pg-isaac constants
    
    Closes: isaac-w9a

--- src/mdm/isaac/thought.clj
+++ src/mdm/isaac/thought.clj
@@ -3,6 +3,8 @@
             [mdm.isaac.config :as config]
             [next.jdbc :as jdbc]))
 
+;; TODO (isaac-sqb) - MDM: Move me to mdm.isaac.thought.core
+
 (defmulti save (fn [_thought] (-> config/active :db :impl)))
 
 ;; region ----- memory -----
@@ -26,29 +28,30 @@
 
 ;; region ----- postgresql -----
 
-;; TODO (isaac-w9a) - MDM: replace me with use of (:db config/active)
-(def pg-root {:dbtype "postgresql" :dbname "postgres" :host "localhost" :port 5432})
+;; TODO (isaac-sqb) - MDM: move this region to mdm.isaac.thought.pg
+
+(defn- pg-root []
+  (assoc (:db config/active) :dbname "postgres"))
 
 (defn pg-create-database [db-name]
-  (jdbc/execute-one! pg-root [(str "CREATE DATABASE " db-name)]))
+  (jdbc/execute-one! (pg-root) [(str "CREATE DATABASE " db-name)]))
 
 (defn pg-drop-database [db-name]
-  (jdbc/execute-one! pg-root [(str "DROP DATABASE IF EXISTS " db-name)]))
-
-(def pg-isaac {:dbtype "postgresql" :dbname "isaac" :host "localhost" :port 5432})
+  (jdbc/execute-one! (pg-root) [(str "DROP DATABASE IF EXISTS " db-name)]))
 
 (defn pg-init [db]
   (jdbc/execute! db ["CREATE EXTENSION IF NOT EXISTS vector"])
   (jdbc/execute! db ["CREATE TABLE IF NOT EXISTS thought (id SERIAL PRIMARY KEY, content TEXT, embedding vector(768))"]))
 
 (defmethod save :postgres [thought]
-  (let [id (:id thought)
+  (let [db (:db config/active)
+        id (:id thought)
         sql (if id
               (format "UPDATE thought SET content = ?, embedding = ?::vector WHERE id = ? RETURNING id, content, embedding")
               (format "INSERT INTO thought (content, embedding) VALUES (?, ?::vector) RETURNING id, content, embedding"))
         result (if id
-                 (jdbc/execute-one! pg-isaac [sql (:content thought) (into-array (:embedding thought)) id])
-                 (jdbc/execute-one! pg-isaac [sql (:content thought) (into-array (:embedding thought))]))]
+                 (jdbc/execute-one! db [sql (:content thought) (into-array (:embedding thought)) id])
+                 (jdbc/execute-one! db [sql (:content thought) (into-array (:embedding thought))]))]
     {:kind :thought
      :id (:thought/id result)
      :content (:thought/content result)
