283000ee08eeae36fc72f3183a31184cca929ae5
Micah <micahmartin@gmail.com>
2026-01-29 20:41:37 -0700

    Refactor thought/pg.clj to use c3kit.bucket API
    Replace raw next.jdbc with bucket's API for CRUD operations:
    - Use db/tx- for save (insert/update)
    - Use db/find- with :order-by {:embedding ['<=> vec]} for similarity search
    - Use db/find- with :where for find-by-type
    
    Retain raw JDBC only for database management (create/drop/init).
    
    Add bucket config helpers to spec_helper for test support.
    
    Closes: isaac-fe2

--- spec/mdm/isaac/spec_helper.clj
+++ spec/mdm/isaac/spec_helper.clj
@@ -1,5 +1,8 @@
 (ns mdm.isaac.spec-helper
-  (:require [mdm.isaac.config :as config]
+  (:require [c3kit.apron.schema :as schema]
+            [c3kit.bucket.api :as db]
+            [mdm.isaac.config :as config]
+            [mdm.isaac.schema.full :as full]
             [speclj.core]))
 
 (defmacro with-config
@@ -7,3 +10,19 @@
    Usage: (with-config {:embedding {:impl :mock}})"
   [config-overrides]
   `(speclj.core/redefs-around [config/active (merge config/active ~config-overrides)]))
+
+(defn bucket-config
+  "Creates a bucket config for the given database name."
+  [db-name]
+  {:impl        :jdbc
+   :dialect     :postgres
+   :host        "localhost"
+   :port        5432
+   :dbtype      "postgresql"
+   :dbname      db-name})
+
+(defn create-bucket
+  "Creates a bucket db instance for the given database name."
+  [db-name]
+  (let [schemas (map schema/normalize-schema full/full-schema)]
+    (db/create-db (bucket-config db-name) schemas)))
