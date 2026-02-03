b3f294749370b4acf0925d733f7acd9650218ced
Micah <micahmartin@gmail.com>
2026-02-03 11:21:57 -0700

    Fix migration to clear existing embeddings before dimension change
    Existing 768-dimension embeddings are incompatible with the new 384-dimension
    column. Clear them first - thoughts will be re-embedded with DJL.

--- resources/mdm/isaac/migrations/20260202_2017_embedding_dimension.clj
+++ resources/mdm/isaac/migrations/20260202_2017_embedding_dimension.clj
@@ -2,7 +2,12 @@
   (:require [c3kit.bucket.jdbc :as jdbc]))
 
 (defn up []
+  ;; Clear existing 768-dimension embeddings - they're incompatible with DJL's 384 dimensions
+  ;; Thoughts will need to be re-embedded with the new provider
+  (jdbc/execute! "UPDATE thought SET embedding = NULL")
   (jdbc/execute! "ALTER TABLE thought ALTER COLUMN embedding TYPE vector(384)"))
 
 (defn down []
+  ;; Clear 384-dimension embeddings before reverting to 768
+  (jdbc/execute! "UPDATE thought SET embedding = NULL")
   (jdbc/execute! "ALTER TABLE thought ALTER COLUMN embedding TYPE vector(768)"))
