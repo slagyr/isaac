1d3a97eb8cff576c67bb8c52126da391cf59cec8
Micah <micahmartin@gmail.com>
2026-02-03 12:15:33 -0700

    Migrate to AWS SDK v2 credentials (isaac-2p1)
    - Replace v1 SDK imports with v2 in aws.clj
    - Add make-credentials-provider function using v2 SDK
    - Update email.clj to use shared aws/make-credentials-provider
    - Remove duplicate credentials code from email.clj
    - Remove AWS SDK v1 dependencies from deps.edn
    - Add tests for make-credentials-provider in aws_spec.clj

--- spec/mdm/isaac/aws_spec.clj
+++ spec/mdm/isaac/aws_spec.clj
@@ -1,8 +1,21 @@
 (ns mdm.isaac.aws-spec
-  (:require [speclj.core :refer :all]))
+  (:require [mdm.isaac.aws :as sut]
+            [mdm.isaac.config :as config]
+            [speclj.core :refer :all])
+  (:import (software.amazon.awssdk.auth.credentials InstanceProfileCredentialsProvider StaticCredentialsProvider)))
 
 (describe "AWS"
 
+  (context "make-credentials-provider"
 
+    (it "returns StaticCredentialsProvider in dev"
+      (with-redefs [config/development? true
+                    sut/access-key      "test-access"
+                    sut/secret-key      "test-secret"]
+        (should (instance? StaticCredentialsProvider (sut/make-credentials-provider)))))
+
+    (it "returns InstanceProfileCredentialsProvider in prod"
+      (with-redefs [config/development? false]
+        (should (instance? InstanceProfileCredentialsProvider (sut/make-credentials-provider))))))
 
 )
