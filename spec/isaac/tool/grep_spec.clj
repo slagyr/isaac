60e097771391d76a1482c37c73e2151a11fe12ab
Micah <micahmartin@gmail.com>
2026-05-11 11:13:18 -0700

    isaac-ms3a: make grep specs hermetic

--- spec/isaac/tool/grep_spec.clj
+++ spec/isaac/tool/grep_spec.clj
@@ -73,14 +73,13 @@
         (should= ["rg" "--color=never" "-l" "--sort" "path" "defn" (str support/test-dir "/src")] @captured)
         (should (str/includes? (:result result) "core.clj"))
         (should (str/includes? (:result result) "util.clj"))))
-
     (it "returns count mode output per file"
       (let [captured (atom nil)
             result   (with-redefs [sut/available? (constantly true)
                                    sut/-run-rg   (fn [cmd]
                                                    (reset! captured cmd)
                                                    {:exit 0 :out "src/core.clj:2\nsrc/util.clj:1" :err ""})]
-                       (sut/grep-tool {"pattern" "defn" "path" (str support/test-dir "/src") "output_mode" "count"}))]
+                        (sut/grep-tool {"pattern" "defn" "path" (str support/test-dir "/src") "output_mode" "count"}))]
         (should-be-nil (:isError result))
         (should= ["rg" "--color=never" "-c" "--sort" "path" "defn" (str support/test-dir "/src")] @captured)
         (should (str/includes? (:result result) "core.clj:2"))
